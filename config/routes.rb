d6169a98f824b01eaa73281943b9dabc5c14517d
Espen <espen.taftoe@gmail.com>
2016-11-02 16:14:59 +0000

    Added home page buttons and icons

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :users do
     resource :profile
   end
+  get 'tjenester' => 'pages#tjenester'
   resources :products
   resources :contacts
   get '/category' => 'pages#category'
