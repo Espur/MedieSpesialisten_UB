cb11a9c2ed77fc16f10bf5e049814126e61fe04e
Espen <espen.taftoe@gmail.com>
2016-11-16 20:21:21 +0000

    Added ID to each avaible service

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,5 +1,12 @@
 class ServicesController< ApplicationController
     
     def index
+        @web_service = Service.find(1)
+        @grafisk_service = Service.find(2)
+        @foto_service = Service.find(3)
+    end
+    
+    def show
+        
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161116113106) do
+ActiveRecord::Schema.define(version: 20161116192413) do
 
   create_table "contacts", force: true do |t|
     t.string   "name"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20161116113106) do
   create_table "services", force: true do |t|
     t.string  "name"
     t.decimal "price"
+    t.string  "service_id"
   end
 
   create_table "users", force: true do |t|
